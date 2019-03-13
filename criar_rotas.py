eba9ae2984df6c724133a2bb8ca892fea5ad21ba
PanicAThePython <nataliaweise@gmail.com>
2019-03-13 19:53:29 -0300

    Enviando meu pi

---
+++ criar_rotas.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/criar_votacao")
+def criar_votacao():
+    return render_template("criar_votacao.html")
+
+@app.route("/votar")
+def votar():
+    return render_template("votar.html")
+
+@app.route("/ver_ranking")
+def ver_ranking():
+    return render_template("ver_ranking.html")
+
+app.run()
