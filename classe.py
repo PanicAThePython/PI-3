d5117193323c2b5eca42e9c69066a6b8002fbf88
PanicAThePython <nataliaweise@gmail.com>
2019-06-02 12:31:14 -0300

    Salvando alterações das páginas

---
+++ classe.py
@@ -0,0 +1,7 @@
+class Usuario():
+
+    def __init__(self, nome, cpf, email, senha):
+        self.nome  = nome
+        self.cpf   = cpf
+        self.email = email
+        self.senha = senha
--- criar_rotas.py
+++ criar_rotas.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, session, redirect
+from classe import *
 
 app = Flask("__name__")
 
@@ -6,9 +7,13 @@ app = Flask("__name__")
 def home():
     return render_template("index.html")
 
-@app.route("/criar_votacao")
-def criar_votacao():
+@app.route("/form_criar_votacao")
+def form_criar_votacao():
     return render_template("criar_votacao.html")
+
+@app.route("/criar_votacao", methods=['post'])
+def criar_votacao():
+    pass
 
 @app.route("/votar")
 def votar():
@@ -18,8 +23,32 @@ def votar():
 def ver_ranking():
     return render_template("ver_ranking.html")
 
-@app.route("/cadastrar")
-def cadastrar():
+@app.route("/form_cadastrar")
+def form_cadastrar():
     return render_template("cadastrar.html")
+
+@app.route("/cadastrar", methods=['post'])
+def cadastrar():
+    pass
+
+@app.route("/form_login")
+def form_login():
+    return render_template("form_login.html")
+
+@app.route("/login", methods=['post'])
+def login():
+    email = request.form["email"]
+    senha = request.form["senha"]
+
+    if email == "bla@gmail.com" and senha == "123":
+        session['usuario'] = email
+        return redirect("/")
+    else:
+        return "erro no login, tente novamente"
+
+@app.route("/logout")
+def logout():
+    session.pop("usuario")
+    return redirect("/")
 
 app.run()
